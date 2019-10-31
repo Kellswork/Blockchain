36eff3a82fc75d31b52ade46d5dc98fbf5c344ef
kellswork <Bighead93+>
2019-10-31 15:25:31 +0100

    add miner.py

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,13 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    # One line version of code to a block
+    block_string = json.dumps(block, sort_keys=True).encode()
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+
+    return  proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +33,13 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    # set a guess
+
+    guess = f'{block_string}{proof}'.encode()
+    # hash the guess
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    # return guess validity
+    return  guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
@@ -42,6 +54,7 @@ if __name__ == '__main__':
     id = f.read()
     print("ID is", id)
     f.close()
+    coins_mined = 0
 
     # Run forever until interrupted
     while True:
@@ -56,7 +69,7 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+         new_proof = proof_of_work(data.get('last_block'))
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -67,4 +80,10 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+
+        if data.get('message') == 'New Block Forged':
+            coins_mined += 1
+            print(f"Total coins mined: {coins_mined}")
+        else:
+            coins_mined += 1
+            print(f"Totak coins mined: {coins_mined}")
